5064abd3a8fb3007b995d1034d9dbc71d4398120
Morteza Q.N <mortezaghorbani3@gmail.com>
2020-04-07 22:25:45 +0430

    complete future contact insert (ArrayList)

--- app/src/main/java/com/example/samplerecyclercontacts/MainActivity.java
+++ app/src/main/java/com/example/samplerecyclercontacts/MainActivity.java
@@ -1,20 +1,39 @@
 package com.example.samplerecyclercontacts;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private ContactsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView= findViewById(R.id.rv_main);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
-        recyclerView.setAdapter(new ContactsAdapter());
+        final RecyclerView recyclerView = findViewById(R.id.rv_main);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        adapter = new ContactsAdapter();
+        recyclerView.setAdapter(adapter);
+
+
+        final EditText fullNameEt = findViewById(R.id.et_main_input);
+        ImageView      addIm      = findViewById(R.id.iv_main_add);
+        addIm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fullNameStr = fullNameEt.getText().toString().trim();
+                if (fullNameStr.length() <= 0) {return;}
+                adapter.addContact(fullNameStr);
+                recyclerView.scrollToPosition(0);
+
+            }
+        });
     }
 }
